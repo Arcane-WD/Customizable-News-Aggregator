7c9c5e1ba0bbbe78aae9105d6ad3b8d4883b62d4
meer-aakif-33 <meeraakif9@gmail.com>
2025-01-24 12:06:53 +0530

    Added DarkMode

---
+++ app/src/components/common/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    <header>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">News</a>Footer</li>
+            <li><a href=""></a>Trending</li>
+            <li><a href="">Profile</a></li>
+        </ul>
+    </header>
+}
